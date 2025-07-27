c78043d1a384c959d4c4cec4a7ea465445be70ae
Nithish <nithishmani79@gmail.com>
2025-07-27 12:21:09 +0530

    Day 7

---
+++ Day7/Program1.cs
@@ -0,0 +1,30 @@
+// using System;
+// public delegate int Admin(int x, int y);
+
+// public delegate void Invoice(int calculate);
+
+
+// class Summa
+// {
+//     static void Main(string[] args)
+//     {
+
+//         Admin ad = Fess;
+
+//         Invoice inv = Invo;
+
+//         int r = ad(200, 400);
+//         inv(ad);
+//     }
+
+//     static int Fess(int x, int y)
+//     {
+//         int r = x + y;
+//         return r;
+//     }
+
+//     static void Invo(int calcuate)
+//     {
+//         Console.WriteLine("The fees is" + calculate);
+//     }
+// }
